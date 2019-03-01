e2c33a80023b6a492a28e16ba275c2847257905c
Matthew Peters <mattphoolishis@gmail.com>
2019-03-01 16:45:42 -0700

    Adds getStudent method.

--- src/main/java/com/students/studentsdemo/controllers/StudentController.java
+++ src/main/java/com/students/studentsdemo/controllers/StudentController.java
@@ -1,8 +1,14 @@
 package com.students.studentsdemo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
-  
+
+  @GetMapping("/students/{id}")
+  public Student getStudent(@PathVariable Long id) {
+    return studentRepository.findById(id);
+  }
 }
