6b82cd96c0402100695a2697831d9145668822db
Matthew Peters <mattphoolishis@gmail.com>
2019-03-05 18:08:04 -0700

    Adds Post Put Delete endpoints.

--- src/main/java/com/students/studentsdemo/controller/StudentController.java
+++ src/main/java/com/students/studentsdemo/controller/StudentController.java
@@ -7,8 +7,12 @@ import com.students.studentsdemo.model.Student;
 import com.students.studentsdemo.repository.StudentRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,7 +21,6 @@ public class StudentController {
   @Autowired
   private StudentRepository studentRepository;
 
-  
   @GetMapping("/api/v1/students")
   public List<Student> getAllStudents() {
     return studentRepository.findAll();
@@ -28,5 +31,24 @@ public class StudentController {
     return studentRepository.findById(id);
   }
 
-  
+  @PostMapping("/api/v1/students")
+  public void addStudent(@RequestBody Student student) {
+    studentRepository.save(student);
+  }
+
+  @PutMapping("/api/v1/students/{id}")
+  public void updateStudent(@PathVariable Long id, @RequestBody Student newStudent) {
+    Optional<Student> student = studentRepository.findById(id);
+    student.map(s -> {
+                  if(newStudent.getName() != null) {
+                    s.setName(newStudent.getName());
+                  }
+                  return studentRepository.save(s);
+              });
+    }
+
+  @DeleteMapping("/api/v1/students/{id}")
+  public void deleteStudent(@PathVariable Long id) {
+    studentRepository.deleteById(id);
+  }
 }
