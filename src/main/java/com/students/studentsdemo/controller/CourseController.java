459ebcca3393caec08a4f75bc56253bbdc3914f7
Matthew Peters <mattphoolishis@gmail.com>
2019-03-05 18:33:51 -0700

    Imports CourseRepository into controller.

--- src/main/java/com/students/studentsdemo/controller/CourseController.java
+++ src/main/java/com/students/studentsdemo/controller/CourseController.java
@@ -1,8 +1,13 @@
 package com.students.studentsdemo.controller;
 
+import com.students.studentsdemo.repository.CourseRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
-  
+
+  @Autowired
+  private CourseRepository courseRepository;
 }
