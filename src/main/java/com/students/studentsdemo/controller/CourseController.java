59ec6ec096d6d782214e832829b20fb7261eb771
Matthew Peters <mattphoolishis@gmail.com>
2019-03-05 19:10:42 -0700

    Adds getCourse to controller.

--- src/main/java/com/students/studentsdemo/controller/CourseController.java
+++ src/main/java/com/students/studentsdemo/controller/CourseController.java
@@ -1,8 +1,13 @@
 package com.students.studentsdemo.controller;
 
+import java.util.Optional;
+
+import com.students.studentsdemo.model.Course;
 import com.students.studentsdemo.repository.CourseRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +15,9 @@ public class CourseController {
 
   @Autowired
   private CourseRepository courseRepository;
+
+  @GetMapping("/api/v1/courses/{id}")
+  public Optional<Course> getCourse(@PathVariable Long id) {
+    return courseRepository.findById(id);
+  }
 }
