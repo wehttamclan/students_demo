ea970f829f625ce2cd48ef1cb7e7d3b90e4d16ff
Matthew Peters <mattphoolishis@gmail.com>
2019-03-05 19:05:18 -0700

    Adds the join table and join columns to Student.

--- src/main/java/com/students/studentsdemo/model/Student.java
+++ src/main/java/com/students/studentsdemo/model/Student.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.validation.constraints.NotBlank;
@@ -20,8 +22,13 @@ public class Student {
   private Long id;
 
   @ManyToMany
+  @JoinTable(
+    name = "student_courses",
+    joinColumns = @JoinColumn(name = "student_id"),
+    inverseJoinColumns = @JoinColumn(name = "course_id")
+  )
   Set<Course> courses;
-  
+
   @NotBlank
   private String name;
 
